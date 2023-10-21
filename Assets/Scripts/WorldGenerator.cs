de20092652790816bc69cc1f18796d53f6747365
williambarkley <72388218+williambarkley@users.noreply.github.com>
2023-10-21 18:44:52 +0200

    Correcta_Generación_Modulos

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -6,6 +6,8 @@ public class WorldGenerator : MonoBehaviour
 {
     public GameObject Tile1;
     public GameObject Tile2;
+    public GameObject Tile3;
+
 
 
     public GameObject StartTile;
@@ -22,20 +24,76 @@ public class WorldGenerator : MonoBehaviour
     private void Update()
     {
         gameObject.transform.position += new Vector3(0, 0, -12 * Time.deltaTime);
+        Tile1.transform.position += new Vector3(0, 0, -12 * Time.deltaTime);
+        Tile2.transform.position += new Vector3(0, 0, -12 * Time.deltaTime);
+        Tile3.transform.position += new Vector3(0, 0, -12 * Time.deltaTime);
+
+
 
         if (transform.position.z <= Index)
         {
-            int RandomInt1 = Random.Range(0, 2);
+            int RandomInt1 = Random.Range(0, 3);
+            
 
-            if (RandomInt1 == 1)
+
+            if (RandomInt1 == 0)
             {
-                GameObject TempTile1 = Instantiate(Tile1, transform);
-                TempTile1.transform.position = new Vector3(0, 0, 105);
+                if (Tile1.active && Tile1.transform.position.z <= -30)
+                {
+                    Tile1.transform.position = new Vector3(0, 0, 105);
+                }
+                else if (!Tile1.active)
+                {
+                    Tile1.SetActive(true);
+                    Tile1.transform.position = new Vector3(0, 0, 105);
+                }
+                else
+                    RandomInt1 = 1;
+
             }
-            else if (RandomInt1 == 0)
+
+
+             if (RandomInt1 == 1)
             {
-                GameObject TempTile2 = Instantiate(Tile2, transform);
-                TempTile2.transform.position = new Vector3(0, 0, 105);
+                if (Tile2.active && Tile2.transform.position.z <= -30)
+                {
+                    Tile2.transform.position = new Vector3(0, 0, 105);
+                }
+                else if (!Tile2.active)
+                {
+                    Tile2.SetActive(true);
+                    Tile2.transform.position = new Vector3(0, 0, 105);
+                }
+                else
+                    RandomInt1 = 2;
+            }
+
+
+            if (RandomInt1 == 2)
+            {
+                if (Tile3.active && Tile3.transform.position.z <= -30)
+                {
+                    Tile3.transform.position = new Vector3(0, 0, 105);
+                }
+                else if (!Tile3.active)
+                {
+                    Tile3.SetActive(true);
+                    Tile3.transform.position = new Vector3(0, 0, 105);
+                }
+                else 
+                {
+                    if (Tile1.active && Tile1.transform.position.z <= -30)
+                    {
+                        Tile1.transform.position = new Vector3(0, 0, 105);
+                    }
+                    else if (!Tile1.active)
+                    {
+                        Tile1.SetActive(true);
+                        Tile1.transform.position = new Vector3(0, 0, 105);
+                    }
+                    
+                }
+            
             }
 
             Index = Index - 100.0f;
