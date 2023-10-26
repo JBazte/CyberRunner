760196b832aa06192ac16cd83bb6317b9b515b8a
AlejandroEscuderoMatas <alex.escudero1100@gmail.com>
2023-10-26 18:51:17 +0200

    fixed

--- Assets/Scripts/Obstacles/WindowMovement.cs
+++ Assets/Scripts/Obstacles/WindowMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WindowMovement : MonoBehaviour
 {
-    private GameObject m_player;
+    private Transform m_player;
     private MeshRenderer m_renderer;
     [SerializeField]
     private float m_distanceToAppear; //Cuando el jugador se encuentre a esta distancia, ocupara el carril
@@ -15,14 +15,14 @@ public class WindowMovement : MonoBehaviour
     {
         m_renderer = GetComponent<MeshRenderer>();
         m_renderer.enabled = false;
-        m_player = GameObject.FindWithTag("Player");
-        distanceFromPlayer = this.transform.position.z - m_player.transform.position.z;
+        m_player = FindObjectOfType<CharacterController>().transform;
+        distanceFromPlayer = this.transform.position.z - m_player.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        distanceFromPlayer = this.transform.position.z - m_player.transform.position.z;
+        distanceFromPlayer = this.transform.position.z - m_player.position.z;
         if(distanceFromPlayer <= m_distanceToAppear)
         {
             //Aparecería la animación de ocupar el carril
