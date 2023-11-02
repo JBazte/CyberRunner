c841cb4d3f806cc78cacd20efc4fde667e432053
AlejandroEscuderoMatas <alex.escudero1100@gmail.com>
2023-11-02 22:37:42 +0100

    Coin model

--- Assets/Scripts/MapElements/Coin.cs
+++ Assets/Scripts/MapElements/Coin.cs
@@ -4,22 +4,6 @@ using UnityEngine;
 
 public class Coin : TemporalSingleton<GameManager>
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    private void FixedUpdate()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>() != null) 
