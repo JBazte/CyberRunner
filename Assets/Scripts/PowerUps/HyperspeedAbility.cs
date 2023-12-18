3096df37e02d98aad25695c4677433e9bf1d4941
AlejandroEscuderoMatas <alex.escudero1100@gmail.com>
2023-12-18 17:43:30 +0100

    Changes for build

--- Assets/Scripts/PowerUps/HyperspeedAbility.cs
+++ Assets/Scripts/PowerUps/HyperspeedAbility.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class HyperspeedAbility : MonoBehaviour
