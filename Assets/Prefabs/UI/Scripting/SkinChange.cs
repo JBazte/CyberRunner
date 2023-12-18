40cf87555dd07f37cecfb8be8b3efc9b79dbaec7
AlejandroEscuderoMatas <alex.escudero1100@gmail.com>
2023-12-18 16:27:31 +0100

    Multiple fixes

--- Assets/Prefabs/UI/Scripting/SkinChange.cs
+++ Assets/Prefabs/UI/Scripting/SkinChange.cs
@@ -5,53 +5,34 @@ public class SkinChange : MonoBehaviour
 {
     // Prefab del nuevo modelo que se asigna por serialización
     [SerializeField]
-    private GameObject nuevoModeloPrefab;
+    private GameObject[] characterModels;
     [SerializeField]
-    private GameObject jugador;
+    private Avatar[] characterAvatar;
+    [SerializeField]
+    private Animator currentAvatar;
 
     UIDocument cosmeticsDoc;
     Button new_skin_btn;
 
-    private void Start()
+    private void OnEnable()
     {
         cosmeticsDoc = GetComponent<UIDocument>();   
         new_skin_btn = cosmeticsDoc.rootVisualElement.Q("Skin_button") as Button;
         new_skin_btn.RegisterCallback<ClickEvent>(CambiarModelo);
-        // Asegúrate de que se haya asignado el prefab desde el Inspector
-        if (nuevoModeloPrefab == null)
-        {
-            Debug.LogError("No se ha asignado el prefab del nuevo modelo.");
-        }
-
-        if (jugador == null)
-        {
-            Debug.LogError("No se ha asignado el GameObject del jugador.");
-        }
-
     }
 
     // Método para cambiar el modelo del jugador
     public void CambiarModelo(ClickEvent evt)
     {
-        if (nuevoModeloPrefab != null && jugador != null)
+        Debug.Log("AAAAAAAAAAAAAAAAAAA");
+        if (characterModels[0].activeSelf)
         {
-            // Crea una nueva instancia del nuevo modelo
-            GameObject nuevoModelo = Instantiate(nuevoModeloPrefab, jugador.transform.position, jugador.transform.rotation);
-
-            // Transfiere la posición y la rotación del jugador al nuevo modelo
-            nuevoModelo.transform.position = jugador.transform.position;
-            nuevoModelo.transform.rotation = jugador.transform.rotation;
-
-            // Destruye el modelo antiguo
-            Destroy(jugador);
-
-            // Actualiza la referencia al jugador con el nuevo modelo
-            jugador = nuevoModelo;
+            currentAvatar.avatar = characterAvatar[0];
+        } else {
+            currentAvatar.avatar = characterAvatar[1];
         }
-        else
-        {
-            Debug.LogError("Asegúrate de asignar el prefab del nuevo modelo y el GameObject del jugador.");
-        }
+        characterModels[0].SetActive(!characterModels[0].activeSelf);
+        characterModels[1].SetActive(!characterModels[1].activeSelf);
     }
     
 }
